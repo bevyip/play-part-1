6f01c39e29ab4ec2c0dd910e2a17e7b147c8e555
Beverly Yip <beverly.yip.8000@gmail.com>
2025-12-18 23:06:19 -0500

    user instructions

--- 6/src/App.jsx
+++ 6/src/App.jsx
@@ -5,12 +5,18 @@ function App() {
   const stickers = Array.from({ length: 9 }, (_, i) => i + 1);
 
   return (
-    <div className="app-container">
-      {stickers.map((num) => (
-        <div key={num} className="sticker-wrapper">
-          <StickerPeel imageSrc={`/${num}.png`} rotate={0} width={200} />
-        </div>
-      ))}
+    <div className="app-wrapper">
+      <div className="app-header">
+        <h1 className="app-title">Cat Sticker Pack</h1>
+        <p className="app-subtitle">peel your favorite sticker</p>
+      </div>
+      <div className="app-container">
+        {stickers.map((num) => (
+          <div key={num} className="sticker-wrapper">
+            <StickerPeel imageSrc={`/${num}.png`} rotate={0} width={200} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
