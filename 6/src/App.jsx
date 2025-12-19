5b26b31fb7bddd5031098b9f411a37096b95fdea
Beverly Yip <beverly.yip.8000@gmail.com>
2025-12-18 22:59:56 -0500

    mobile friendly

--- 6/src/App.jsx
+++ 6/src/App.jsx
@@ -1,32 +1,13 @@
 import StickerPeel from "./StickerPeel.jsx";
+import "./App.css";
 
 function App() {
   const stickers = Array.from({ length: 9 }, (_, i) => i + 1);
 
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 1fr)",
-        gridTemplateRows: "repeat(3, 1fr)",
-        gap: "40px",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-        width: "100vw",
-        padding: "40px",
-        background: "#0f0f0f",
-      }}
-    >
+    <div className="app-container">
       {stickers.map((num) => (
-        <div
-          key={num}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
+        <div key={num} className="sticker-wrapper">
           <StickerPeel imageSrc={`/${num}.png`} rotate={0} width={200} />
         </div>
       ))}
